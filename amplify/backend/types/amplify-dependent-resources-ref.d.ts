cc37365bb72ad8110a8aa5c4284a48182d4d92d5
Idriss Ibrahim <idriss_ibrahim@idriss.ibrahim-HP-ProBook-440-G1>
2023-08-17 16:40:05 +0100

    DropBox Frontend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mydropbox20cb9db5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { withAuthenticator, Button } from '@aws-amplify/ui-react';
+import { Auth } from 'aws-amplify'; // Import Auth from aws-amplify
 
 function App() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = async () => {
+    try {
+      if (!file) {
+        alert('Please select a file to upload.');
+        return;
+      }
+
+      // Use AWS SDK or Amplify Storage to upload the file to S3 here
+      // You can use the Amplify Storage.put() method for this purpose
+
+      alert('File uploaded successfully.');
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      alert('An error occurred while uploading the file.');
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My DropBox App!</h1>
+        <div>
+          <input type="file" onChange={handleFileChange} />
+          <Button onClick={handleFileUpload}>Upload File</Button>
+        </div>
+        <p></p>
+        <div className="SignOut">
+        <Button onClick={handleSignOut}>Sign Out</Button>
+      </div>
       </header>
+      
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
